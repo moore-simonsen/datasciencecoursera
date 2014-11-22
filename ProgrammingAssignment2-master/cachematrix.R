986656a5fbcc8d48d58bf82864df72576c0ca290
mooresimonsen <s.mooresimonsen@gmail.com>
2014-11-23 00:30:57 +0100

    myfirstcache

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,31 @@
+## Catching the inverse of a Matrix
+## Creates a special matrix object that can catch its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setsolve <- function(solve) m <<- solve
+	getsolve <- function() m
+	list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+}
+
+
+## Computes the inverse of the special matrix returned by makeCacheMatrix above
+
+cacheSolve <- function(x, ...) {
+	m <- x$getsolve()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setsolve(m)
+	m
+}
+        ## Return a matrix that is the inverse of 'x'
+}
